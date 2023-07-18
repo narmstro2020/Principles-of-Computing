51a8f4a8f881eb8eb548c808c6740911969666e1
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-07-18 14:33:51 -0400

    Unit 03 - Lesson 04 - Notes

---
+++ Unit 02 - Basics of Programming with Python/Lesson 04 - Variables and Data Types/Notes/main.py
@@ -0,0 +1,50 @@
+#Created a variable named greeting and set its value to Hello World
+# The = sign is the assignment operator.  It assigns the value on the left to the item on the right.  
+greeting1 = "Hello World"
+greeting2 = "Hi again!"
+print(greeting1)
+print(greeting2)
+
+an_int = 3
+an_int_type = type(an_int)
+print("an_number_type is", an_int_type)
+
+a_float = -2.5
+a_float = type(a_float)
+print("a_float_type is", a_float)
+
+greeting1_type = type(greeting1)
+print("greeting1_type is" , greeting1_type)
+
+large_int = 12_356_789
+print(large_int)
+
+hex_num = 0xF3
+print(hex_num)
+
+oct_num = 0o37
+print(oct_num)
+
+bin_num = 0b10101011110110
+print(bin_num)
+
+# 3.84 * 10^4
+sci_notation_number = 3.84e4
+print(sci_notation_number)
+
+'''
+Python Data Types
+Covered today
+Text Type:	str
+Numeric Types:	int, float
+
+Covered at a later date
+Numeric Types:	complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+
+'''
