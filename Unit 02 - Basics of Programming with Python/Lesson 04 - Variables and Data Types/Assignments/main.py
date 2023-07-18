6233f141196259e0c712d5e25796834acd7d2d60
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-07-18 14:42:47 -0400

    Unit 02 - Lesson 04 - Assignment

---
+++ Unit 02 - Basics of Programming with Python/Lesson 04 - Variables and Data Types/Assignments/main.py
@@ -0,0 +1,14 @@
+'''
+Initialize two variables to store the number of apples and number of oranges in your store. 
+You have 20 apples and 15 oranges.
+Then print out how many you have of each. 
+Then set the number of oranges to 0 and print out the values again.
+
+Your output must be in this format:
+
+Number of apples: __
+Number of oranges: __
+Number of apples: __
+Number of oranges: __
+
+'''
