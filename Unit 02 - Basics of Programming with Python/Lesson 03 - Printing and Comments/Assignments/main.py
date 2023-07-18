ac8d9968e5c928a2e6f7ff1fc9e96a6f1fc2029c
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-07-18 13:02:58 -0400

    Unit 02 - Lesson 03 - Assignment

---
+++ Unit 02 - Basics of Programming with Python/Lesson 03 - Printing and Comments/Assignments/main.py
@@ -0,0 +1,10 @@
+''' 
+Using the print function, write a program that prints out your name on one line, 
+and then on the next line says a hobby you have.
+'''
+
+# Your output must be in this format:
+# The underlines represent your unique responses.  
+
+# My name is _____________
+# I like __________________
