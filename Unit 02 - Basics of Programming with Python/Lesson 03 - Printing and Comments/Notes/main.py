7aed9e0c315023a47e68e202dea292cbf8989b7a
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-07-18 12:44:01 -0400

    Unit 02 - Lesson 03 Notes

---
+++ Unit 02 - Basics of Programming with Python/Lesson 03 - Printing and Comments/Notes/main.py
@@ -0,0 +1,33 @@
+#This is the print function.  It prints the literal string in single or double quotes
+print("Hello World")
+print("Hi Again")
+#You can separate words by a comma.  A space appear automatically.
+print("Hi", "Again")
+#You can change the word separator by adding sep=""
+print("Hi", "Again", sep="-")
+#You can change the ending part by end.  Defaults to a new line.  
+print("Hi", "Again", end="")
+print("Something")
+print("print")
+#Single quotes are allowed
+print('single quotes')
+#Use the \ as the escape character
+#prints a single quote
+print('\'')
+#prints a double quote
+print("\"")
+#prints 1 backslash
+print("\\")
+#prints 2 backslashes.
+print("\\\\")
+#print a tab
+print("\tHello")
+#prints a new line
+print("\n")
+print("Hello")
+
+'''
+This is a multiline comment.  
+Feel free to space them out
+Starts and ends with three single quote marks
+'''
