ec87263a7442bd820f4689d1ccf8060d57966035
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-07-18 18:01:21 -0400

    Unit 02 - Lesson 05 - Assignments

---
+++ Unit 02 - Basics of Programming with Python/Lesson 05 - Mathematical Operators/Assignments/assignment1.py
@@ -0,0 +1,11 @@
+'''
+Write a program that asks the user how many frisbees they would like to buy, 
+and then prints out the total cost. 
+
+Since we know nothing of user input at this point just a random number for the amount of frisbees to buy
+
+You should declare a constant at the top of your program called COST_OF_FRISBEE and set it equal to $15. 
+Remember, constants should be formatted with all capital letters.
+
+
+'''
---
+++ Unit 02 - Basics of Programming with Python/Lesson 05 - Mathematical Operators/Assignments/assignment2.py
@@ -0,0 +1,14 @@
+'''
+Write a program that asks the user how many miles they ran and how many minutes it took to run that far. 
+
+Since we know nothing of user input at this point just use random numbers for the miles and minutes
+
+Convert their speed to miles per hour and print out their speed.
+
+Sample Output:  
+
+How many miles did you run? 3
+How many minutes did it take you? 60
+Speed in mph: 3.0
+
+'''
