7a0a5534f635316ce7d1c86776670a24c4c145f7
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-07-24 17:43:36 -0400

    Unit 02 - Lesson 08 - Notes

---
+++ Unit 02 - Basics of Programming with Python/Lesson 08 - String Operators/Notes/notes.py
@@ -0,0 +1,28 @@
+print("Hello", "World", sep=".", end="#")
+print("I'm a computer")
+print("HI again")
+
+a = "Nicholas Armstrong"
+num_letters_in_a = len(a)
+print(num_letters_in_a)
+
+b = "Mr. "
+name = b + a
+print(name)
+
+#string concatenation
+first = "Nicholas"
+last = "Armstrong"
+full_name = first + " " + last
+print(full_name)
+
+print(first + " " + last)
+
+#string copying
+
+print(first * 2)
+
+a = 4.6
+b = "Hi "
+print(b + str(a))
+print(b, a)
