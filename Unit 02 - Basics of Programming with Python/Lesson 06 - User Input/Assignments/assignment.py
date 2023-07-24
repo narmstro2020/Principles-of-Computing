61f149fc5ac997a04efb9a88d7d042af3f1da5fe
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-07-24 16:33:06 -0400

    Unit 02 - Lesson 06 - Assignments

---
+++ Unit 02 - Basics of Programming with Python/Lesson 06 - User Input/Assignments/assignment.py
@@ -0,0 +1,7 @@
+# You have the opportunity to meet some droids and Wookiees!
+
+# Prompt the user for their name, then how many droids, and then how many Wookiees they want to meet. 
+# Print out the name that was given, as well as how many droids and Wookiees they wanted to meet.
+# Example output
+# Sean wants to meet 3 droids and 2 Wookiees.
+# Sean is the entered name, 3 is the entered number for droids, 2 is the entered number of Wookiees.
