31c7ad02c50f5e5d26c24283a2b44c463a01564c
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-07-24 16:57:16 -0400

    Unit 02 - Lesson 07 - Assignments

---
+++ Unit 02 - Basics of Programming with Python/Lesson 07 - Type Casting/Assignments/assignment.py
@@ -0,0 +1,17 @@
+'''
+
+Create a temperature converter.  
+
+Ask the user for the temperature in Fahrenheit as a variable F
+Convert this to a float
+Use the following math to figure out the degrees in Celsius
+C = 5 * (F - 32) / 9
+C is the Celsius temperature.  
+print out the result.  
+
+Then do the the same for Celsius.  
+Ask the user for the temp in Celsius and convert to Fahrenheit
+The Formula is
+F = 1.8 * C + 32
+
+'''
