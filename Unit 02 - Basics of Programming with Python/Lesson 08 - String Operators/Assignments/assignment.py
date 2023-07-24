1d0894cb56310c75f1b0c4451240d84500f866b0
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-07-24 17:46:34 -0400

    Unit 02 - Lesson 08 - Assignments

---
+++ Unit 02 - Basics of Programming with Python/Lesson 08 - String Operators/Assignments/assignment.py
@@ -0,0 +1,14 @@
+'''
+Write a program that takes your first name as user input
+Then ask the user for the number of repeats
+(Don't forget to convert this to an int)
+Then print out the first name joined to itself repeat number of times
+
+example output
+
+Nick was the input
+3 was the number of repeats
+
+NickNickNick
+
+'''
