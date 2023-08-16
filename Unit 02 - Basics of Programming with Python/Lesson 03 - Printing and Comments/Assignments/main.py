bfeb74f201fb1e0c9c22a626b45cb9c4f188afc1
bostisco000 <142335343+bostisco000@users.noreply.github.com>
2023-08-16 11:59:09 -0400

    Update main.py

--- Unit 02 - Basics of Programming with Python/Lesson 03 - Printing and Comments/Assignments/main.py
+++ Unit 02 - Basics of Programming with Python/Lesson 03 - Printing and Comments/Assignments/main.py
@@ -6,5 +6,5 @@ and then on the next line says a hobby you have.
 # Your output must be in this format:
 # The underlines represent your unique responses.  
 
-# My name is _ptint("Scott bostic")______
-# I like _print ("basketball")_______________
+# My name is _______
+# I like ________________
