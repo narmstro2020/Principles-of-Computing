39660aed90cc9c09fe24906b66bdf5c6893d50c5
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-08-23 07:56:35 -0400

    U2L7 worked

--- Unit 02 - Basics of Programming with Python/Lesson 07 - Type Casting/Assignments/assignment.py
+++ Unit 02 - Basics of Programming with Python/Lesson 07 - Type Casting/Assignments/assignment.py
@@ -1,5 +1,4 @@
 '''
-
 Create a temperature converter.  
 
 Ask the user for the temperature in Fahrenheit as a variable F
@@ -13,5 +12,10 @@ Then do the the same for Celsius.
 Ask the user for the temp in Celsius and convert to Fahrenheit
 The Formula is
 F = 1.8 * C + 32
-
 '''
+
+fah = float(input("Enter the temp in degrees F. ")) 
+cel = 5 * (fah - 32) / 9
+print("The temperature in degrees C is:", cel)
+
+#YOUDO:  the same but for the celsius to fahrenheit
