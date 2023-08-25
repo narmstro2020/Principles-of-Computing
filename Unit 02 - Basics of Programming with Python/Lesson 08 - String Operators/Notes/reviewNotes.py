56802e9436241af8aefd53b3d7b6e3d9edbae832
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-08-25 07:43:17 -0400

    unit 2 review notes

---
+++ Unit 02 - Basics of Programming with Python/Lesson 08 - String Operators/Notes/reviewNotes.py
@@ -0,0 +1,32 @@
+print("Hello", "World", sep="-", end="\n")
+
+a = "Hi There"
+b = 4
+c = -5.3
+
+'''
+All of the math operators.  Review notes in lesson 5 for all of them. 
+
+'''
+
+an_integer_string = "7"
+an_integer = int(an_integer_string)
+
+a_float_string = "7.5"
+a_float = float(a_float_string)
+
+a_number = -8.2
+a_number_string = str(a_number)
+
+user_inputted_text = input("Enter some text: ")
+
+a_float_string_length = len(a_float_string)
+
+type_of_a_float = type(a_float)
+
+first = "Nicholas"
+last = "Armstrong"
+space = " "
+full_name = first + space + last
+
+copied_first = first * 5
