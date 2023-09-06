696baff6dcf268070697bed3ea579c63ae54db42
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-09-06 08:01:23 -0400

    U4L1 Assignment walkthrough

--- Unit 04 - Basic Graphics Programming in Python/Lesson 01 - Creating the Canvas/Assignments/assignment1.py
+++ Unit 04 - Basic Graphics Programming in Python/Lesson 01 - Creating the Canvas/Assignments/assignment1.py
@@ -2,3 +2,19 @@
 Starting with a 200x200 red canvas, use the create_line() function to draw two horizontal and two vertical lines
 on the canvas to create a Tic Tac Toe board.  Try your best to center your board and leave space around the outside like so
 '''
+
+from tkinter import *
+
+root = Tk()
+
+screen = Canvas(root, width=200, height=200, bg="#FF0000")
+screen.pack()
+
+screen.create_line(0, 67, 200, 67)   #horizontal line
+# add another horizontal line
+
+screen.create_line(67, 0, 67, 200)   #vertical line
+# add another vertical line
+
+
+mainloop()
--- Unit 04 - Basic Graphics Programming in Python/Lesson 01 - Creating the Canvas/Assignments/assignment2.py
+++ Unit 04 - Basic Graphics Programming in Python/Lesson 01 - Creating the Canvas/Assignments/assignment2.py
@@ -5,3 +5,16 @@ can be the first letter of your name, or any letter that you think will be chall
 Feel free to draw out your letter on paper first if it will help you.  
 
 '''
+
+from tkinter import *
+
+root = Tk()
+
+screen = Canvas(root, width=200, height=200, bg="#FF0000")
+screen.pack()
+
+
+screen.create_line(50, 200, 50, 0, fill="#FFFFFF", width=3.0)
+
+
+mainloop()
