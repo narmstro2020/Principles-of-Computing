fc25a68e3dfa01ce0ff5740b81f0e1feb0935840
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-09-29 07:42:59 -0400

    another change

--- Unit 07 - Loops and Iteration/Lesson 01 - While Loops/assignment.py
+++ Unit 07 - Loops and Iteration/Lesson 01 - While Loops/assignment.py
@@ -6,7 +6,7 @@ secret_number = randint(0, 20)
 user_guess = input("Enter a guess between 0 and 20")
 user_guess_num = int(user_guess)
 number_of_guesses = 1
-while user_guess != secret_number:
+while user_guess_num != secret_number:
     number_of_guesses += 1
     #print some message that says you guessed wrong.  
     #ask the user again to enter a guess and store in user_guess
