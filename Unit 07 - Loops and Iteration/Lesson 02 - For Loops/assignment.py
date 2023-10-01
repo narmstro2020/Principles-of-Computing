3b06c6d245477e093299db4de96bd58790e780ac
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-10-01 12:24:46 -0400

    Unit 7 Lesson 02

---
+++ Unit 07 - Loops and Iteration/Lesson 02 - For Loops/assignment.py
@@ -0,0 +1,11 @@
+#write a program that adds up all of the numbers from 
+#0 to 100
+# you should get 5050
+# use a for loop
+
+sum = 0
+for number in range():   #You figure out the range
+    #You figure out what to do to sum every time in the loop
+    pass # Delete when you've finished the for loop
+
+print(sum)
---
+++ Unit 07 - Loops and Iteration/Lesson 02 - For Loops/notes.py
@@ -0,0 +1,22 @@
+# counter = 0
+# while counter <= 10:
+#     print(counter)
+#     counter += 1
+
+# print("All Done")
+
+# for counter in range(11):
+#     print(counter)
+
+# for counter in range(0, 11, 2):
+#     print(counter)
+
+for timer in range(10, -1, -1):
+    print(timer)
+
+print("Blast Off")
+
+word = "Principles of Computing"
+
+for letter in word:
+    print(letter)
