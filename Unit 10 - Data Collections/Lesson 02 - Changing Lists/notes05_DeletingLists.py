42a6244c9652c6f74a0dad2bc89efc0065d79410
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-11-06 08:55:01 -0500

    Unit 10 Lesson 2

---
+++ Unit 10 - Data Collections/Lesson 02 - Changing Lists/notes05_DeletingLists.py
@@ -0,0 +1,18 @@
+#Deleting Lists
+
+# top_cities = ['New York City', 'Los Angeles', 'Singapore', 'Chicago', 'Houston', 'Phoenix']
+# del top_cities[2]
+# print(top_cities)
+
+# top_cities = ['New York City', 'Los Angeles', 'Singapore', 'Chicago', 'Houston', 'Phoenix']
+# del top_cities[3:]
+# print(top_cities)
+
+# top_cities = ['New York City', 'Los Angeles', 'Singapore', 'Chicago', 'Houston', 'Phoenix']
+# del top_cities[:]
+# print(top_cities)
+
+top_cities = ['New York City', 'Los Angeles', 'Singapore', 'Chicago', 'Houston', 'Phoenix']
+del top_cities
+print(top_cities)
+
