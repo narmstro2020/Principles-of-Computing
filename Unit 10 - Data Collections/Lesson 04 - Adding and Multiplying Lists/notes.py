29db98e02b95ec18974762333bbc1190f15adfb7
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-11-15 13:47:00 -0500

    Unit 10 Lesson 4

---
+++ Unit 10 - Data Collections/Lesson 04 - Adding and Multiplying Lists/notes.py
@@ -0,0 +1,19 @@
+# book_ratings = [7, 9, 5, 6, 8]
+# print(book_ratings)
+# book_ratings.append(4)
+# print(book_ratings)
+
+# append(4)
+
+# book_ratings = [7, 9, 5, 6, 8]
+# print(book_ratings)
+# book_ratings.insert(1, 10)
+# print(book_ratings)
+
+# list_us = ['New York City', 'Chicago']
+# list_uk = ['London', 'Bristol']
+# list_all = list_us + list_uk
+# print(list_all)
+
+list_numbers = [0, 1] * 10
+print(list_numbers)
