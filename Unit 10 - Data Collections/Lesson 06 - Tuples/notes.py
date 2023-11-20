6223551d730b4b9e162b8cc1fda45423c62f963e
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-11-20 08:06:59 -0500

    assignment update

--- Unit 10 - Data Collections/Lesson 06 - Tuples/notes.py
+++ Unit 10 - Data Collections/Lesson 06 - Tuples/notes.py
@@ -1,11 +1,11 @@
 empty_tuple = ()
-
-one_el_tuple_a = (1, )
+print(empty_tuple)
+one_el_tuple_a = (1,)
 print(one_el_tuple_a)
 print(type(one_el_tuple_a))
 
 one_el_tuple_b = 1,
-
+print(one_el_tuple_b)
 three_el_tuple = 1, 2, 3,
 
 print(three_el_tuple)
@@ -18,45 +18,32 @@ user_data = ('Katya', 'Russian', 1980)
 
 # del user_data[0]
 
-# print(user_data[0])
+print(user_data[0])
 
 # user_data[0] = 'Mark'
 
-# print(user_data[0])
+message = 'welcome'
 
-
-# message = 'welcome'
-
-# message = 'Welcome'
+message = 'Welcome'
 
 # message[0] = 'w'
 
-# user_data = ('John', 'American', 1964)
-# print(len(user_data))
-
-# user_data = ('John', 'American', 1964)
-# if 'American' in user_data:
-#     print('This person comes from the US!')
-
-# user_data = ('John', 'American', 1964)
-# for element in user_data:
-#     print(element)
-
-# user_data = ('John', 'American', 1964) + ('teacher', 'male')
-# print(user_data)
-
-# numbers = (0, 1) * 10
-# print(numbers)
-
-male_name = ['Adam', 'Jerry', 'Mark']
-
-berlin_temps = [13.0, 17.5, 12.0]
+user_data = ('John', 'American', 1964)
+print(len(user_data))
 
 user_data = ('John', 'American', 1964)
+if 'American' in user_data:
+    print('This person comes from the US!')
 
-first = 5
-second = 7
-first, second = second, first
+user_data = ('John', 'American', 1964)
+for element in user_data:
+    print(element)
+
+user_data = ('John', 'American', 1964) + ('teacher', 'male')
+print(user_data)
+
+numbers = (0, 1) * 10
+print(numbers)
 
 
 
