19174290f610b61a1563765262dfb3c2d7b5b65e
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-11-19 19:02:07 -0500

    Unit 10 Lesson 6

---
+++ Unit 10 - Data Collections/Lesson 06 - Tuples/notes.py
@@ -0,0 +1,62 @@
+empty_tuple = ()
+
+one_el_tuple_a = (1, )
+print(one_el_tuple_a)
+print(type(one_el_tuple_a))
+
+one_el_tuple_b = 1,
+
+three_el_tuple = 1, 2, 3,
+
+print(three_el_tuple)
+
+user_data = ('John', 'American', 1964)
+
+user_data = ('Katya', 'Russian', 1980)
+
+# user_data.append('teacher')
+
+# del user_data[0]
+
+# print(user_data[0])
+
+# user_data[0] = 'Mark'
+
+# print(user_data[0])
+
+
+# message = 'welcome'
+
+# message = 'Welcome'
+
+# message[0] = 'w'
+
+# user_data = ('John', 'American', 1964)
+# print(len(user_data))
+
+# user_data = ('John', 'American', 1964)
+# if 'American' in user_data:
+#     print('This person comes from the US!')
+
+# user_data = ('John', 'American', 1964)
+# for element in user_data:
+#     print(element)
+
+# user_data = ('John', 'American', 1964) + ('teacher', 'male')
+# print(user_data)
+
+# numbers = (0, 1) * 10
+# print(numbers)
+
+male_name = ['Adam', 'Jerry', 'Mark']
+
+berlin_temps = [13.0, 17.5, 12.0]
+
+user_data = ('John', 'American', 1964)
+
+first = 5
+second = 7
+first, second = second, first
+
+
+
