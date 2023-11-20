0e0d1b8b0ec24d7ff54f2dbeed17975e5d198e0c
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-11-20 11:53:38 -0500

    final update 10.6

--- Unit 10 - Data Collections/Lesson 06 - Tuples/notes.py
+++ Unit 10 - Data Collections/Lesson 06 - Tuples/notes.py
@@ -1,13 +1,14 @@
-empty_tuple = ()
-print(empty_tuple)
-one_el_tuple_a = (2, )
-print(one_el_tuple_a)
-print(type(one_el_tuple_a))
+# empty_tuple = ()
+# print(empty_tuple)
 
-one_el_tuple_b = 1,
-print(one_el_tuple_b)
-three_el_tuple = 1, 2, 3,
-print(three_el_tuple)
+# one_el_tuple_a = (2, )
+# print(one_el_tuple_a)
+# print(type(one_el_tuple_a))
+
+# one_el_tuple_b = 1,
+# print(one_el_tuple_b)
+# three_el_tuple = 1, 2, 3,
+# print(three_el_tuple)
 
 user_data = ("John", 'American', 1964)
 
@@ -17,30 +18,29 @@ user_data = ('Katya', 'Russian', 1980)
 
 # del user_data[0]
 
-print(user_data[0])
+# print(user_data[0])
 
 # user_data[0] = 'Mark'
 
-message = 'welcome'
+# message = 'welcome'
 
-message = 'Welcome'
+# message = 'Welcome'
 
 # message[0] = 'w'
 
-user_data = ('John', 'American', 1964)
-print(len(user_data))
+# user_data = ('John', 'American', 1964)
+# print(len(user_data))
 
-user_data = ('John', 'American', 1964)
-if 'American' in user_data:
-    print('This person comes from the US!')
+# user_data = ('John', 'American', 1964)
+# if 'American' in user_data:
+#     print('This person comes from the US!')
 
-user_data = ('John', 'American', 1964)
-for element in user_data:
-    print(element)
+# user_data = ('John', 'American', 1964)
+# for element in user_data:
+#     print(element)
 
-
-user_data = user_data + ('teacher', 'male')
-print(user_data)
+# user_data =  user_data + ('teacher', 'male')
+# print(user_data)
 
 numbers = (0, 1) * 10
 print(numbers)
