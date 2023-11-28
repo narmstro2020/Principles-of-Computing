7c9adab6404e83f3b0739f0a834ad8688dfab46c
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-11-28 06:48:26 -0500

    assignment updates

--- Unit 10 - Data Collections/Lesson 08 - Further String Features/notes.py
+++ Unit 10 - Data Collections/Lesson 08 - Further String Features/notes.py
@@ -3,7 +3,7 @@
 
 # print(fav_band[:6])
 
-# # fav_band[6] = 'M'
+# fav_band[6] = 'M'
 
 # text = "please capitlise me"
 # text_cap = text.upper()
@@ -13,11 +13,11 @@ user_number = input('Please provide a number: ')
 if user_number.isnumeric():
     print('Thank you, that\'s a correct number!')
 elif user_number[0] == "-" and user_number[1:].isnumeric():
-    print("This number is negative")
+    print("You have a negative whole number")
 elif user_number.count(".") == 1 and user_number.replace(".", "").isnumeric():
     print("You have a positive decimal number")
-elif user_number.count(".") == 1 and user_number[0] == "-" and user_number[1:].replace(".", "").isnumeric():
-    print("you have a negative decimal number")
+elif user_number.count(".") == 1 and user_number[0] == "-" and user_number.replace(".", "")[1:].isnumeric():
+    print("You have a negative decimal number")
 else:
     print('Sorry,', user_number, 'is not a number!')
 
