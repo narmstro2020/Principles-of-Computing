3a8757ec14c75e3ec51f671e915f56ded4626134
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-11-30 12:01:07 -0500

    Updates

--- Unit 11 - Functions/Lesson 01 - Functions, Parameters, Default Values/notes.py
+++ Unit 11 - Functions/Lesson 01 - Functions, Parameters, Default Values/notes.py
@@ -4,6 +4,7 @@
 # greet()
 # greet()
 
+
 # def get_average(input_numbers):
 #     sum = 0.0
 #     for number in input_numbers:
@@ -16,7 +17,7 @@
 
 # get_average()
 
-# get_average(5)
+#get_average(5)
 
 # def print_letter_count(text, letter):
 #     counter = 0
@@ -25,13 +26,13 @@
 #             counter += 1
 #     print('Number of', letter, 'is', counter)
 
-# # print_letter_count('Welcome', 'e')
+# print_letter_count('Welcome', 'e')
 
-# # print_letter_count('People say nothing is impossible, but I do nothing every day.', 'a')
+# print_letter_count('People say nothing is impossible, but I do nothing every day.', 'a')
 
-# # print_letter_count('e', 'Welcome')
+# print_letter_count('e', 'Welcome')
 
-# # # print_letter_count(text='Welcome', letter='e')
+# print_letter_count(text='Welcome', letter='e')
 
 # print_letter_count(letter='e', text='Welcome')
 
@@ -44,8 +45,37 @@ def print_letter_count(text='This is the default string to search', letter='a'):
             counter += 1
     print('Number of', letter, 'is', counter)
 
-print_letter_count('How many letters a are here?')
+# print_letter_count('How many letters a are here?')
 
-print_letter_count()
+# print_letter_count()
 
-print_letter_count('Search here', letter='a')
+# print_letter_count('Search here', letter='a')
+
+# # #Python
+# def greet():
+#     print("Hi There")
+
+# # #JavaScript
+# function greet(){
+#     console.log("Hi There");
+# }
+
+# # #C
+# void greet(){
+#     puts("Hi There");
+# }
+
+# # #C++
+# void greet(){
+#     cout << "Hi There" << endl;
+# }
+
+# # #C#
+# public void greet(){
+#     Console.Writeline("Hi There");
+# }
+
+# # #Java
+# public void greet(){
+#     System.out.println("Hi There");
+# }
