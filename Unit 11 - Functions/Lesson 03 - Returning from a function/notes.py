d92b3df43c6f470ca32b9711a748ca05e7f3e715
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-12-04 09:23:59 -0500

    updates

--- Unit 11 - Functions/Lesson 03 - Returning from a function/notes.py
+++ Unit 11 - Functions/Lesson 03 - Returning from a function/notes.py
@@ -1,29 +1,32 @@
-def get_average(input_numbers):
-    sum = 0.0
-    for number in input_numbers:
-        sum += number
-    average = sum / len(input_numbers)
-    return average
+# def get_average(input_numbers):
+#     sum = 0.0
+#     for number in input_numbers:
+#         sum += number
+#     average = sum / len(input_numbers)
 
-print('The average is:', get_average([5.0, 3.5, 7.8, 9.9, 10.0]))
+# # average = get_average([5.0, 3.5, 7.8, 9.9, 10.0])
 
-average = get_average([5.0, 3.5, 7.8, 9.9, 10.0])
-if average > 5.0:
-    print('The average is too high!')
+# # print('The average is:', average)
 
-def get_average(input_numbers):
-    sum = 0.0
-    for number in input_numbers:
-        sum += number
-    average = sum / len(input_numbers)
-    return average
-    print('Show me!')
+# average = get_average([5.0, 3.5, 7.8, 9.9, 10.0])
+# if average > 5.0:
+#     print('The average is too high!')
 
-get_average([2])
+# def get_average(input_numbers):
+#     sum = 0.0
+#     for number in input_numbers:
+#         sum += number
+#     average = sum / len(input_numbers)
+#     return average
+#     print('Show me!')
+
+# average = get_average([2])
+
+# print('The average is:', average)
 
 def is_first_last_equal(number_list):
     if len(number_list) == 0:
-        return
+        return 
     if (number_list[0] == number_list[-1]):
         return True
     else:
