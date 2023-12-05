2a542ba11cdd1cee61c80303ab363f5fd7b7c8f6
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-12-05 07:59:28 -0500

    updates

---
+++ Unit 11 - Functions/Lesson 04 - The None Type/assignment.py
@@ -0,0 +1,12 @@
+def safeDivide(dividend, divisor):
+    if divisor is 0:
+        ## return something to handle this
+        pass    #remove the pass when done. 
+    else:
+        ## return something to handle this.  
+        pass  # remove the pass when done.
+
+#3 / 0
+x = safeDivide(3, 0)   # Should give you None   because 3 / 0
+y = safeDivide(3, 2)   # Should give you 1.5    because 3 / 2
+z = safeDivide(2, 3)   # Should give you 0.666666666667  because 2 / 3
