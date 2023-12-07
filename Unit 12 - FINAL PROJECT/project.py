625f678bca4f8806adc3cbf346593a48cdebc6fc
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-12-07 11:39:39 -0500

    updates

--- Unit 12 - FINAL PROJECT/project.py
+++ Unit 12 - FINAL PROJECT/project.py
@@ -12,44 +12,8 @@ start_x = 0
 start_y = 0
 size = 20
 
-small_mario = [
-    [g, g, g, r, r, r, r, r, r, g, g, g, g, g, g, g],  #row0
-    [g, g, r, r, r, r, r, r, r, r, r, r, g, g, g, g],  #row1
-    [],  #row2
-    [],  #row3
-    [],  #row4
-    [],  #row5
-    [],  #row6
-    [],  #row7
-    [],  #row8
-    [],  #row9
-    [],  #row10
-    [],  #row11
-    [],  #row12
-    [],  #row13
-    [],  #row14
-    [],  # row15
-]
-
 screen = Canvas(root, width=320, height=320, bg=clear_color)
 screen.pack()
 
-def draw_rectangle(x, y, width, height, color):
-    ending_x = x + width
-    ending_y = y + height
-    screen.create_rectangle(x, y, ending_x, ending_y, fill=color)
-    
-def draw_sprite(sprite):
-        x = start_x
-        y = start_y
-        for row in sprite:
-            for color in row:
-                draw_rectangle(x, y, size, size, color)
-                x += size
-            x = start_x
-            y += size
-            
-
-draw_sprite(small_mario)
 
 mainloop()
