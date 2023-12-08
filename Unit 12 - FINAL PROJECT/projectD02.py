f4c493f0550bfb5e148d48beca9f30c33f5d312d
Nicholas Armstrong <narmstro@warren.k12.in.us>
2023-12-08 11:56:47 -0500

    updates

--- Unit 12 - FINAL PROJECT/projectD02.py
+++ Unit 12 - FINAL PROJECT/projectD02.py
@@ -3,37 +3,37 @@ from tkinter import *
 root = Tk()
 
 k = "#000000"  #black
-r = "#d62b18"  #red
-b = "#876f16"  #brown
-s = "#fa9644"  #mario skin tone
+r = "#d62b18"  #mario's red
+b = "#876f16"  #mario's brown
+s = "#fa9644"  #mario's skin tone
 g = "#6185f8"  #background
-clear_color = "#4d2d44"  # that purpley color
+clear_color = "#4d2d44"  #purpley color
 
 start_x = 0
 start_y = 0
 size = 20
 
-# finish this mario sprite
+#finish this mario sprite
 small_mario = [
-    [g, g, g, r, r, r, r, r, r, g, g, g, g, g, g, g],  # row 0
-    [g, g, r, r, r, r, r, r, r, r, r, r, g, g, g, g],  # row 1
-    [],  # row 2
-    [],  # row 3
-    [],  # row 4
-    [],  # row 5
-    [],  # row 6
-    [],  # row 7
-    [],  # row 8
-    [],  # row 9
-    [],  # row 10
-    [],  # row 11
-    [],  # row 12
-    [],  # row 13
-    [],  # row 14
-    [],  # row 15
+    [g, g, g, r, r, r, r, r, r, g, g, g, g, g, g, g], #row 0
+    [g, g, r, r, r, r, r, r, r, r, r, r, g, g, g, g], #row 1
+    [], #row 2
+    [], #row 3
+    [], #row 4
+    [], #row 5
+    [], #row 6
+    [], #row 7
+    [], #row 8
+    [], #row 9
+    [], #row 10
+    [], #row 11
+    [], #row 12
+    [], #row 13
+    [], #row 14
+    [], #row 15
 ]
 
-#finish the goomba sprite
+# finish this goomba sprite
 goomba = [
     [],  # row 0
     [],  # row 1
@@ -73,7 +73,6 @@ def draw_rectangle(x, y, width, height, color):
     ending_y = y + height
     screen.create_rectangle(x, y, ending_x, ending_y, fill=color)
 
-
 def draw_sprite(sprite):
     x = start_x
     y = start_y
@@ -83,7 +82,7 @@ def draw_sprite(sprite):
             x += size
         x = start_x
         y += size
-
+        
 
 draw_sprite(goomba)
 
